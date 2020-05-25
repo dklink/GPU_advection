c9d30f76333eaecf23d05e0d72b2e2700ee8715a
dklink <dougdklink3@gmail.com>
2020-05-25 19:26:07 -0400

    some more hycom data processing

--- data/processing.py
+++ data/processing.py
@@ -1,11 +1,19 @@
 import xarray as xr
+from tqdm import tqdm
+import glob
 
-
-def process_hycom(path='uv_2015_1_3d.nc'):
+def process_hycom_file(path='uv_2015_1_3d.nc'):
     """loads hycom data, assigns coords, changes the longitude from 0,360 to -180,180"""
     ds = xr.open_dataset(path)
     ds['lon'] = ((ds.lon+180) % 360) - 180
     ds = ds.assign_coords({'x': ds.lon, 'y': ds.lat, 'z': ds.depth})
     ds = ds.roll(x=len(ds.x) // 2, roll_coords=True)
-    ds.to_netcdf('hycom_formatted.nc')
+
+    filename = path.split('/')[-1]
+    ds.to_netcdf(f'{filename[:-3]}.formatted{filename[-3:]}')
     return ds
+
+
+def process_all_hycom_files():
+    for path in tqdm(glob.glob('../../trashtracker/utils/get hycom/nc/uv_2015*.nc')):
+        process_hycom_file(path)
